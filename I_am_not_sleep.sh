feedd091823eafb2b929e429279baec2631ea0b5
Jermian <jreedsentry@gmail.com>
2023-07-26 22:32:49 +0000

    I_am_not_sleep_push

---
+++ I_am_not_sleep.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script Name:                  My Second Bash Script
+# Author:                       Jermain Reed
+# Date of latest revision:      7/25/2023
+# Purpose:                      To learn bash prints/ create a print to screen
+# Declaration of variables
+
+
+
+# Main
+echo “I am working perfectly!”
+
+
+bas# End
