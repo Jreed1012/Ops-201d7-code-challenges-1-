1e22940ca398819b7172c4d24e7408f229238744
Jermian <jreedsentry@gmail.com>
2023-07-26 22:30:50 +0000

    Helloworld push

---
+++ Hello_world.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  My First Bash Script
+# Author:                       Jermain Reed
+# Date of latest revision:      7/25/2023
+# Purpose:                      To learn bash prints/ create a print to screen
+#Resource used                   https://chat.openai.com/
+# Declaration of variables
+
+
+
+# Main
+echo "Hello, World!"
+
+
+
+# End
